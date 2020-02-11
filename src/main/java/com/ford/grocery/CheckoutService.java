b02f070314ed036dfb813ba7a268f28464da277f
Gustavo Almagro Rojas <galmagro@gmail.com>
2020-02-11 21:40:49 +0000

    Added more tests, covered edge case where product is added with wrong unit type

--- src/main/java/com/ford/grocery/CheckoutService.java
+++ src/main/java/com/ford/grocery/CheckoutService.java
@@ -1,5 +1,7 @@
 package com.ford.grocery;
 
+import static java.lang.String.format;
+
 public class CheckoutService {
 
     private StockItemRepository stockItemRepository;
@@ -12,10 +14,19 @@ public class CheckoutService {
         Receipt receipt = new Receipt();
         for(ShoppingBasket.BasketItem basketItem: basket) {
             //will use the product name as the key
-            stockItemRepository.findStockItem(basketItem.getProduct())
-                    .ifPresent(stockItem -> receipt.addItems(basketItem.getQuantity(), stockItem));
+            stockItemRepository
+                    .findStockItem(basketItem.getProduct())
+                    .ifPresent(stockItem -> this.addItemToReceipt(stockItem, basketItem, receipt));
         }
         return receipt;
+    }
+
+    private void addItemToReceipt(final StockItem stockItem, final ShoppingBasket.BasketItem basketItem, final Receipt receipt ) {
+        if(stockItem.getUnit().equals(basketItem.getUnit())){
+            receipt.addItems(basketItem.getQuantity(), stockItem);
+        } else {
+            throw new IllegalArgumentException(format("invalid unit {} for product {}", basketItem.getUnit(), basketItem.getProduct()));
+        }
     }
 
 }
--- src/test/java/com/ford/grocery/ShoppingTest.java
+++ src/test/java/com/ford/grocery/ShoppingTest.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 
 public class ShoppingTest {
 
+    private static final double DELTA = 0.00001;
+
     private CheckoutService checkoutService;
 
     @Before
@@ -19,14 +21,34 @@ public class ShoppingTest {
     }
 
     @Test
-    public void testBasketWithoutDiscounts(){
+    public void shouldCalculateTotalWithoutDiscounts(){
         ShoppingBasket basket = new ShoppingBasket();
         basket.add(1, TIN, "soup");
         basket.add(2, LOAF, "bread");
         basket.add(1, BOTTLE, "milk");
 
         Receipt receipt = checkoutService.checkout(basket);
-        assertEquals("basket total price doesn't match", 0.65 + (2 * 0.80) + 1.30,  receipt.getTotal(), 0.00001);
+        assertEquals("basket total price doesn't match", 0.65 + (2 * 0.80) + 1.30,  receipt.getTotal(), DELTA);
+    }
+
+    @Test
+    public void shouldCalculateTotalWithoutDiscountsWhenAddingSameProductMultipleTimes(){
+        ShoppingBasket basket = new ShoppingBasket();
+        basket.add(1, LOAF, "bread");
+        basket.add(1, BOTTLE, "milk");
+        basket.add(1, LOAF, "bread");
+
+        Receipt receipt = checkoutService.checkout(basket);
+        assertEquals("basket total price doesn't match", (2 * 0.80) + 1.30,  receipt.getTotal(), DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAcceptInvalidProductUnits(){
+        ShoppingBasket basket = new ShoppingBasket();
+        basket.add(1, LOAF, "bread");
+        basket.add(1, TIN, "milk");
+
+        checkoutService.checkout(basket);
     }
 
 }
